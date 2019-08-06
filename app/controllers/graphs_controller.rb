01dba07d41e5f5f46437ee8a4c8f60ce052efc4f
Calvin Tan <hello@calvin-tan.com>
2019-08-07 01:08:53 +0800

    Turn excel data in a hash for rendering chart

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -7,14 +7,9 @@ class GraphsController < ApplicationController
 
   def show
     @graph = Graph.find(params[:id])
-    @graph_data = @graph.datatables
-
-    @data_array = []
-    @graph_data.each do |data|
-      @temp_array = []
-      @temp_array << data.key
-      @temp_array << data.value
-      @data_array << @temp_array
-    end
+    @graph_data = @graph.datatables[0]
+    keys = @graph_data.key.split(", ")
+    values = @graph_data.value.split(", ").map(&:to_i)
+    @data_hash = Hash[keys.zip(values)]
   end
 end
