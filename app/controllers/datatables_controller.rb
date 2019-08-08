a961b5c198cad69a4c7b3c3e712c7c5c96805bec
Zhou Sun <ZHOUSUN@USC.EDU>
2019-08-08 16:13:55 +0800

    put the new bar in the right place

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -1,6 +1,7 @@
 class DatatablesController < ApplicationController
   def index
     @datatables = Datatable.where(graph_id: params[:graph_id])
+    @datatable = Datatable.new
     @qr = RQRCode::QRCode.new("bizwiz.herokuapp.com/graphs/17/datatables/")
     @data_array = []
     @datatables.each do |data|
