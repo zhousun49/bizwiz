5beb77744e594274823dc308e69820d635bd5d6e
Zhou Sun <ZHOUSUN@USC.EDU>
2019-08-08 17:27:47 +0800

    working version

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -3,7 +3,6 @@ class DatatablesController < ApplicationController
     @datatables = Datatable.where(graph_id: params[:graph_id])
     @datatable = Datatable.new
     @graph = @datatables.first.graph
-    @qr = RQRCode::QRCode.new("bizwiz.herokuapp.com/graphs/17/datatables/")
     @data_array = []
     @datatables.each do |data|
       @temp_array = []
