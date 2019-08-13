2d9bc18d2d04921097999f2fa4b0bfceb593dae4
Calvin Tan <hello@calvin-tan.com>
2019-08-13 15:16:53 +0800

    Revert to previous commit

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -1,6 +1,7 @@
 class DatatablesController < ApplicationController
   def index
     @datatables = Datatable.where(graph_id: params[:graph_id])
+    @datatable = Datatable.new
     @graph = @datatables.first.graph
     total_value = 0
     @datatables.each { |e| total_value += e.value }
